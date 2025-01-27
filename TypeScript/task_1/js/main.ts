0e3837d734b2544c6af4992318eb61705bebddb7
Yannis95200 <ranguin.yannis98@gmail.com>
2025-01-27 17:17:27 +0100

    Extending the Teacher class

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -7,13 +7,6 @@ interface Teacher {
     [propName: string]: any;
 }
 
-const teacher3: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-};
-
-console.log(teacher3);
-
+interface Director extends Teacher {
+    numberOfReports: number;
+}
